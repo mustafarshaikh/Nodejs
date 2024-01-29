909e3ec8be132c5ec4964f3085f1a61a267aaf5a
mustafarshaikh <mustafarshaikh@gmail.com>
2024-01-29 18:30:57 +0400

    4

--- index.js
+++ index.js
@@ -1,51 +1,14 @@
-for(let i=1;i<=100;i++){
-    console.log("hello", i );
+//arrrays
+//Arrays are linear: 
+let marks = [0,1,2,3,4,5,6];
+console.log(marks);
+console.log(marks.length) //property
+for(i=0;i<marks.length;i++){
+    console.log(i)
 }
-let a = 5;
-while(a <= 100){
-    console.log(a);
-    a++;
+marks[5] = 66;
+console.log(marks);
+for(i=0;i<marks.length;i++){
+    console.log(marks[i])
 }
-do{
-    console.log("first", a);
-    a++
-}while(a <= 2)
-///String for-of
-let uname = "mushy";
-let size = 0;
-for(let b of uname){
-    console.log(b);
-    size++;
-}
-console.log(size);
-//for -n loop
-const student = {
-    sname : "muhsy",
-    number: 45,
-    ispass : true
-};
-for(i in student){
-    console.log(i);
-    console.log(student[i]);
-}
-for (i=0;i<100;i++){
-    if(i%2 === 0){
-        console.log(i); 
-    }
-}
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-
-console.log(randomNumber);
-
-// let uprompt = prompt("guess the number");
-// while (uprompt != randomNumber){
-//     if(uprompt > randomNumber){
-//         uprompt = prompt(" you were higher please retry");
-//     }else {
-//         uprompt = prompt(" you were lower please retry");
-//     }
-// }
-// alert("coreect answer is", uprompt); 
-
-let str = "hello7";
-console.log(str.length);
+//iterable strings object array
